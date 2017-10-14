51d7e335680156f9634c4c29485a543fd9bd749a
Kyle Sykes <kyle.sykes@careotter.com>
2017-10-13 23:16:43 -0500

    Add Procfile, fix app.py

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
     """Says happy birthday to a user"""
     return {'message': 'Happy {0} Birthday {1}!'.format(age, name),
             'took': float(hug_timer)}
+
+if __name__ == '__main__':
+    user.interface.local()
