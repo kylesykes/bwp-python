63dd5fbd75942a0484f474510947db002a8116dd
Kyle Sykes <kyle.sykes@careotter.com>
2017-10-14 10:57:37 -0500

    Add get/post as verbs to login endpoint

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ super_secret_key = 'abcd1234'
 #login authentication
 authentication = hug.http(requires=hug.authentication.basic(hug.authentication.verify('shelter', '1234')))
 
+@authentication.get('/login')
 @authentication.post('/login')
 def login(user: hug.directives.user, key=super_secret_key):
     return {"message": "Successfully authenticated with user: {0}".format(user),
