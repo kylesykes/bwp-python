4136be1820d06a56e4bf7e030d2182e6c42b8ba0
Kyle Sykes <kyle.sykes@careotter.com>
2017-10-14 10:49:53 -0500

    Add basic authentication

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 """First API, local access only"""
 import hug
 
+demo_logins = {
+    'username' : 'shelter',
+    'password' : '1234'
+}
+
+super_secret_key = 'abcd1234'
+
+#login authentication
+authentication = hug.http(requires=hug.authentication.basic(hug.authentication.verify('shelter', '1234')))
+
+@authentication.post('/login')
+def login(user: hug.directives.user, key=super_secret_key):
+    return {"message": "Successfully authenticated with user: {0}".format(user),
+            "key" : key}
 
 @hug.get(examples='name=Timothy&age=26')
 @hug.local()
@@ -8,6 +22,13 @@ def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
     """Says happy birthday to a user"""
     return {'message': 'Happy {0} Birthday {1}!'.format(age, name),
             'took': float(hug_timer)}
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     user.interface.local()
