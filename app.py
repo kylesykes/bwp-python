b62b6bb51483a58e861a5703ee672906a254f0b9
Kyle Sykes <kyle.sykes@careotter.com>
2017-10-14 19:22:37 -0500

    Add linking route

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import redis
 import json
 import base64
 from xmlrpc.client import Binary
+import uuid
 
 
 
@@ -25,6 +26,12 @@ user = redis.from_url(REDIS_URL, db=user_db_id)
 pet = redis.from_url(REDIS_URL, db=pet_db_id)
 
 super_secret_key = 'abcd1234'
+
+"""
+HELPER FUNCTIONS
+"""
+def get_uuid():
+    return str(uuid.uuid4())
 
 #login authentication
 authentication = hug.http(requires=hug.authentication.basic(hug.authentication.verify('shelter', '1234')))
@@ -59,8 +66,11 @@ USER ROUTES
 
 @hug.post('/owner', requires=cors_support)
 def create_owner(body):
+    if body.get('username', None) is None:
+        return {"message" : "Bad person, you need a username"}
     username = body.get('username', None)
     body['role'] = "owner"
+    body['adopted_pets'] = []
     #post owner to owner redis database
     user.set(username, json.dumps(body))
     return body
@@ -82,24 +92,78 @@ PET ROUTES
 
 @hug.post('/pet', requires=cors_support)
 def create_pet(body):
-    name = body.get('name', None)
+    pet_uuid = get_uuid()
+    pet_name = body.get('name', None)
+    temp_key = '{}:{}'.format(pet_name, 'test')
+    
+    body['adopted'] = False
+    
     #post pet to pet redis database
-    pet.set(name, json.dumps(body))
+    pet.set(temp_key, json.dumps(body))
+    # add id to response
+    body['pet_id'] = pet_uuid
     return body
 
 
 @hug.get('/pet', requires=cors_support)
-def get_pet(name: hug.types.text = None):
+def get_pet(pet_id: hug.types.text = None):
     # get pet from user redis database
-    if name is None:
+    if pet_id is None:
         # get all pet records
         return pet.keys()
     else:
-        return json.loads(pet.get(name))
+        return json.loads(pet.get(pet_id))
 
 
 """
-CREATE KENNEL USER
+LINKING ROUTES
+"""
+
+@hug.post('/link', requires=cors_support)
+def link_owner_pet(body):
+    pet_id = body['pet_id']
+    owner_id = body['owner_username']
+
+    #get and alter owner dict
+    owner_object = json.loads(user.get(owner_id))
+    adopted_pet_list = owner_object.get('adopted_pets', [])
+    if pet_id not in adopted_pet_list:
+        adopted_pet_list.append(pet_id)
+    owner_object['adopted_pets'] = adopted_pet_list
+
+    # get and update pet object to show adopted
+    pet_object = json.loads(pet.get(pet_id))
+    pet_object['adopted'] = True
+    pet_object['adopted_by'] = owner_id
+
+    # send updates to redis
+    pet.set(pet_id, json.dumps(pet_object))
+    user.set(owner_id, json.dumps(owner_object))
+    return {
+            'owner': owner_object,
+            'pet' : pet_object
+    }
+
+
+"""
+DOCUMENTS
+"""
+
+
+@hug.post('/document', requires=cors_support)
+def get_document():
+    test_document_path = 'docs/rabies_cert.pdf'
+
+    with open(test_document_path, "rb") as f:
+        encodedZip = base64.b64encode(f.read())
+    return {
+            'message' : "Successfully transmitted",
+            'base64' : encodedZip.decode("utf-8") 
+    }
+
+
+"""
+CREATE DEMO STUFF
 """
 @hug.post('/demo_setup', requires=cors_support)
 def demo_setup():
@@ -114,16 +178,7 @@ def demo_setup():
     }
 
 
-@hug.post('/document', requires=cors_support)
-def get_document():
-    test_document_path = 'docs/rabies_cert.pdf'
 
-    with open(test_document_path, "rb") as f:
-        encodedZip = base64.b64encode(f.read())
-    return {
-            'message' : "Successfully transmitted",
-            'base64' : encodedZip.decode("utf-8") 
-    }
 
 
 """
